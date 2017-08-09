1d3584dce68cc53742701414e309a5d731123ed4
Ray Zane <ray@promptworks.com>
2017-08-09 16:38:55 -0400

    Support lteq and gteq

--- lib/advanced/active_record.rb
+++ lib/advanced/active_record.rb
@@ -14,15 +14,27 @@ module Advanced
         end
       end
 
-      def where_lt(param, column: param)
-        define_search param, [param] do |value|
-          where(arel_table[column].lt(value))
-        end
+      def where_lt(*args)
+        where_arel(:lt, *args)
       end
 
-      def where_gt(param, column: param)
+      def where_gt(*args)
+        where_arel(:gt, *args)
+      end
+
+      def where_lteq(*args)
+        where_arel(:lteq, *args)
+      end
+
+      def where_gteq(*args)
+        where_arel(:gteq, *args)
+      end
+
+      private
+
+      def where_arel(meth, param, column: param)
         define_search param, [param] do |value|
-          where(arel_table[column].gt(value))
+          where(arel_table[column].send(meth, value))
         end
       end
     end
